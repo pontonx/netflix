eb23f820e3fc8a880a5f955dec0e7bfb3e4bea61
ponton <31003388+pontonx@users.noreply.github.com>
2022-01-08 21:26:48 +0100

    Add files via upload

--- moviefinder.js
+++ moviefinder.js
@@ -1,5 +1,6 @@
 let findButton = document.querySelector("#searchbutton");
 let film = document.querySelector(".films");
+let details = document.querySelector("#details");
 
 findButton.addEventListener("click", SearchMovie);
 
@@ -10,6 +11,7 @@ function SearchMovie() {
     fetch('https://api.themoviedb.org/3/search/movie?api_key=3d569174bc2693edc30f38898abab3e4&language=en-US&query='+search+'&page=1&include_adult=false')
     .then(response => response.json())
     .then(data => {
+        console.log("MOVIES FOUND: ");
         console.log(data);
 
         film.innerHTML = "";
@@ -20,12 +22,36 @@ function SearchMovie() {
             
             if(element.poster_path)
             film.innerHTML += 
-            "<div class='film'> " +
+            "<div class='film' onclick='showDetails("+element.id+");'> " +
             "<img class='filmImage' src='https://www.themoviedb.org/t/p/w1280"+element.poster_path+"'</img>" +
             "<h1>"+element.original_title+"</h1>" +
-            "<p>&#11088;: " + element.vote_average + 
+            "<p>&#11088;: " + element.vote_average + "/10</p>" + 
             "</div>";
 
         }
     });
+}
+
+function showDetails(movie) {
+    fetch('https://api.themoviedb.org/3/movie/'+movie+'?api_key=3d569174bc2693edc30f38898abab3e4&language=en-US')
+    .then(response => response.json())
+    .then(movieData => {
+        console.log(movieData.title.toString().toUpperCase() +" DETAILS: ");
+        console.log(movieData);
+
+        details.innerHTML = 
+        "<div class='x'>❌</div>" +
+        "<h1>" + movieData.title + "</h1>" +
+        "<p>Overview: " + movieData.overview + "</p>" +
+        "<p>Release date: " + movieData.release_date + "</p>" +
+        "<p>Runtime: " + (parseFloat(movieData.runtime, 2)/60).toFixed(2) + "h</p>";
+
+        let x = document.querySelector(".x");
+
+        x.addEventListener("click", function() {
+            details.classList.toggle("hidden");
+        });
+
+        details.classList.toggle("hidden");
+    })
 }
